3a3d6735f9284505c9840892c7648adf655bd24a
giorgoscodes <07digits@gmail.com>
2025-02-20 18:11:52 +0200

    added the role selection feature in the signup page

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,12 +1,38 @@
 import "../styles/SignUp.css";
 import { Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import axiosInstance from '../axiosConfig';
+
 
 const SignUp = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Form submitted');
+        const response = axiosInstance.post('/users', formData);
+        if (response.status === 200) {
+            console.log('User created successfully');
+            navigate("/home");
+        } else {
+            console.error('Error creating user');
+            alert('An error occurred while signing up.');
+        }
+
+
+
     };
 
     return (
@@ -19,17 +45,23 @@ const SignUp = () => {
             <div className="sign-up">
                 <div className="header">
                     <h1>Create an account</h1>
-                    <p>Enter username and password</p>
+                    <p>Enter username, password and select a role</p>
                 </div>
 
                 <div className="form">
                     <form onSubmit={handleSubmit}>
 
                         <label>Username</label>
-                        <input type="text" placeholder="username" />
+                        <input type="text" name="username" value={formData.username} onChange={handleChange} placeholder="username" />
 
                         <label>Password</label>
-                        <input type="password" placeholder="password" />
+                        <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="password" />
+
+                        <label>Role</label>
+                        <select name="role" value={formData.role} onChange={handleChange}>
+                            <option value="Admin">Admin</option>
+                            <option value="Customer">Customer</option>
+                        </select>
 
                         <button>Sign Up</button>
                     </form>
